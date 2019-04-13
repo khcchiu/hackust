2526c81ac8fe9e9df4e50a9c44f06724617aed13
chiuhc24 <kennethchiuhc@gmail.com>
2019-04-13 22:22:49 +0800

    Data gathered

--- main/forms.py
+++ main/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from main.models import *
 
-class AnalysisForm(forms.ModelForm):
+class AnalysisForm(forms.Form):
+    """
     location = forms.CharField(max_length=40)
     hashtag = forms.CharField(max_length=40)
     
     class Meta:
         model = Analysis
         fields = ('location', 'hashtag',)
+    """
+    def __init__(self, *args, **kwargs):
+        super(AnalysisForm, self).__init__(*args, **kwargs)
+        self.fields['dataset'] = forms.ChoiceField(label='Dataset', required=True,
+                                            choices=[('1', 'sample'), ('2', 'all_premade'), ('3', 'all')], widget=forms.RadioSelect)
